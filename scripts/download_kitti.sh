2e3b373c98fb19eab548a18a2b75466fece6ef94
Md. Arafat Islam <98916082+Thorfast191@users.noreply.github.com>
2026-02-11 21:53:25 +0600

    creating the base for MMDE

--- scripts/download_kitti.sh
+++ scripts/download_kitti.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
+
+# Install kaggle
+pip install -q kaggle
+
+# Make kaggle directory
+mkdir -p ~/.kaggle
+
+# Copy kaggle.json (must be uploaded manually first)
+cp kaggle.json ~/.kaggle/
+chmod 600 ~/.kaggle/kaggle.json
+
+# Download dataset
 kaggle datasets download -d klemenko/kitti-depth
-unzip kitti-depth.zip -d data/kitti
+
+# Unzip to correct location
+mkdir -p /content/kitti
+unzip -q kitti-depth.zip -d /content/drive/MyDrive/kitti
